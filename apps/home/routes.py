e8a2029ea3184dfbb635c5b1dd5830adbf976c63
Emmanuel-Sigei <emksigei@gmail.com>
2023-11-07 20:38:56 +0300

    Finally Fixed the displaying of Youtube Videos

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -1,14 +1,7 @@
-# -*- encoding: utf-8 -*-
-"""
-Python_Assignment
-"""
-
 from apps.home import blueprint
 from flask import render_template, request
 from flask_login import login_required
 from jinja2 import TemplateNotFound
-
-# Import the function to retrieve YouTube videos
 from apps.youtube_utils import search_youtube_videos
 
 @blueprint.route('/index')
@@ -23,42 +16,27 @@ def route_template(template):
         if not template.endswith('.html'):
             template += '.html'
 
-        # Detect the current page
         segment = get_segment(request)
 
-        # Serve the file (if exists) from app/templates/home/FILE.html
         return render_template("home/" + template, segment=segment)
     except TemplateNotFound:
         return render_template('home/page-404.html'), 404
     except:
         return render_template('home/page-500.html'), 500
 
-def search_youtube_videos(query, max_results=10):
-    youtube = build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
-
+@blueprint.route('/gender_based_violence_videos.html')  # Define a new route for displaying videos
+@login_required
+def display_youtube_videos():
     try:
-        search_response = youtube.search().list(
-            q=query,
-            type="video",
-            maxResults=max_results
-        ).execute()
+        # Define the query to search for YouTube videos
+        query = "Gender Based Violence in Kenya"
 
-        videos = []
+        # Call the function to retrieve YouTube videos
+        videos = search_youtube_videos(query, max_results=10)
 
-        for search_result in search_response.get("items", []):
-            video = {
-                "title": search_result["snippet"]["title"],
-                "video_id": search_result["id"]["videoId"]
-            }
-            videos.append(video)
-
-        print("YouTube API Response:", search_response)  # Add this line to log the API response
-        return videos
-
-    except Exception as e:
-        print("Error fetching YouTube videos:", str(e))
-        return []
-
+        return render_template('home/gender_based_violence_videos.html', videos=videos, segment='videos')
+    except:
+        return render_template('home/page-500.html'), 500
 
 # Helper - Extract current page name from request
 def get_segment(request):
--- apps/youtube_utils.py
+++ apps/youtube_utils.py
@@ -1,26 +1,28 @@
-import os
+# Import the necessary libraries
 from googleapiclient.discovery import build
 from dotenv import load_dotenv
+import os
 
 # Load your YouTube API key from the .env file
 load_dotenv()
 YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
 
-# Print the loaded YouTube API Key
-print("Loaded YouTube API Key:", YOUTUBE_API_KEY)
-
 def search_youtube_videos(query, max_results=10):
     youtube = build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
 
     try:
+        # Execute a search request
         search_response = youtube.search().list(
             q=query,
             type="video",
-            maxResults=max_results
+            maxResults=max_results,
+            part="snippet"  # You should add the "part" parameter
         ).execute()
 
-        videos = []
+        # Log the API response for debugging
+        print("YouTube API Response:", search_response)
 
+        videos = []
         for search_result in search_response.get("items", []):
             video = {
                 "title": search_result["snippet"]["title"],
@@ -28,12 +30,11 @@ def search_youtube_videos(query, max_results=10):
             }
             videos.append(video)
 
-        # Debug: Print the retrieved videos
-        print("Retrieved Videos:", videos)
-
         return videos
 
     except Exception as e:
         print("Error fetching YouTube videos:", str(e))
         return []
 
+
+# You can call this function to get a list of video dictionaries
